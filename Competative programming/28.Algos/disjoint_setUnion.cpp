fa533148231c5013866b4176d020b0857a7a5e15
SachinyadavAug20 <samtagon777@gmail.com>
2026-06-04 19:48:47 +0530

    Solved: Find in mountain array by using 3 binary search | disjoint set union basic implementation

--- Competative programming/28.Algos/disjoint_setUnion.cpp
+++ Competative programming/28.Algos/disjoint_setUnion.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 const int N=1e5+10;
 int parent[N];
+int sz[N]; // size of tree=no of node
 void make(int v){
     parent[v]=v;
+    sz[v]=1; // initilized
 }
 int find(int v){
     if(v==parent[v])return v;
@@ -15,17 +17,38 @@ int find(int v){
         //     a=parent[v];
         // }
         // return a;
-    return find(parent[v]);
+    return parent[v]=find(parent[v]); // path compression
 }
 void Union(int a,int b){
     // join root
     a=find(a); // root 1
     b=find(b); // root 1
     if(a!=b){ // if not in same tree
+        if(sz[a]<sz[b]) swap(a,b); // union by sz
+        // a is bigger than b
         parent[b]=a;
+        sz[a]+=sz[b];
     }
 }
 
 int main(){
+    // give n node and k edges give no of connected components
+    int n,k;
+    cin>>n>>k;
+    for(int i=0;i<=n;i++){
+        make(i);
+    }
+    while(k--){
+        int u,v;
+        cin>>u>>v;
+        Union(u,v);
+    }
+    int connect_ctn=0; // = no of unique parent
 
+    int ans=0;
+    for(int i=0;i<n;i++){
+        if(i==find(i)) ans++;
+    }
+    cout<<ans<<endl;
+    return 0;
 }
--- Competative programming/_Problems_/find_in_mountain_array.cpp
+++ Competative programming/_Problems_/find_in_mountain_array.cpp
@@ -1,53 +1,7 @@
+// https://leetcode.com/problems/find-in-mountain-array/submissions/2022360667/
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-
-void printprimefactors(vector<pair<int, int>> a);
-void printVect2D(vector<pair<pair<int, int>, pair<int, int>>> a);
-void printVectSS(vector<pair<pair<int, int>, int>> a);
-
-void printImage(vector<vector<int>> a) {
-  for (auto v : a) {
-    for (int n : v) {
-      cout << n << " ";
-    }
-    cout << endl;
-  }
-}
-void printVector(vector<int> a) {
-  for (int n : a) {
-    cout << n << " ";
-  }
-  cout << endl;
-}
-
-int main(int argn, char *argv[]) { return 0; }
-
-void printVect(vector<unsigned> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << a[i] << " ";
-  }
-  cout << endl;
-}
-void printVect2D(vector<pair<pair<int, int>, pair<int, int>>> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << "(" << a[i].first.first << "," << a[i].first.second << ") " << "("
-         << a[i].second.first << "," << a[i].second.second << ") " << endl;
-  }
-}
-void printVectSS(vector<pair<pair<int, int>, int>> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << a[i].first.first << " " << a[i].first.second << " > " << a[i].second
-         << endl;
-  }
-}
-
-void printprimefactors(vector<pair<int, int>> a) {
-  for (auto pr : a) {
-    cout << pr.first << "^" << pr.second << " ";
-  }
-  cout << endl;
-}
 
 class Solution {
   // moutain array 1,2,3,4,5,3,2,1
@@ -110,3 +64,4 @@ public:
     return -1;
   }
 };
+
