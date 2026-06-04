5d2cc1cb259ce53cd13db39103fc125b091a00b0
SachinyadavAug20 <samtagon777@gmail.com>
2026-06-04 22:49:47 +0530

    Solved: contains duplicate using sliding window and hsh

--- Competative programming/_Problems_/contains_duplicate_in_range.cpp
+++ Competative programming/_Problems_/contains_duplicate_in_range.cpp
@@ -1,62 +1,7 @@
+// https://leetcode.com/problems/contains-duplicate-i/
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-
-void printprimefactors(vector<pair<int, int>> a);
-void printVect2D(vector<pair<pair<int, int>, pair<int, int>>> a);
-void printVectSS(vector<pair<pair<int, int>, int>> a);
-
-void printImage(vector<vector<int>> a) {
-  for (auto v : a) {
-    for (int n : v) {
-      cout << n << " ";
-    }
-    cout << endl;
-  }
-}
-void printVector(vector<int> a) {
-  for (int n : a) {
-    cout << n << " ";
-  }
-  cout << endl;
-}
-
-int main(int argn, char *argv[]) { return 0; }
-
-void printVect(vector<unsigned> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << a[i] << " ";
-  }
-  cout << endl;
-}
-void printVect2D(vector<pair<pair<int, int>, pair<int, int>>> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << "(" << a[i].first.first << "," << a[i].first.second << ") " << "("
-         << a[i].second.first << "," << a[i].second.second << ") " << endl;
-  }
-}
-void printVectSS(vector<pair<pair<int, int>, int>> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << a[i].first.first << " " << a[i].first.second << " > " << a[i].second
-         << endl;
-  }
-}
-
-void printprimefactors(vector<pair<int, int>> a) {
-  for (auto pr : a) {
-    cout << pr.first << "^" << pr.second << " ";
-  }
-  cout << endl;
-}
-
-
-
-
-
-
-
-
-
 
 class SolutionMY {
 public:
@@ -101,3 +46,4 @@ public:
         return false;
     }
 };
+
