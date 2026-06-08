ef7c472c0c6ab9606e65e27e4982a7a55bba0cc9
SachinyadavAug20 <samtagon777@gmail.com>
2026-06-08 21:57:09 +0530

    Solved:reverse linked list using iterative and recursive method

--- Competative programming/_Problems_/reverse_linked_list.cpp
+++ Competative programming/_Problems_/reverse_linked_list.cpp
@@ -1,59 +1,8 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-
-void printprimefactors(vector<pair<int, int>> a);
-void printVect2D(vector<pair<pair<int, int>, pair<int, int>>> a);
-void printVectSS(vector<pair<pair<int, int>, int>> a);
-
-void printImage(vector<vector<int>> a) {
-  for (auto v : a) {
-    for (int n : v) {
-      cout << n << " ";
-    }
-    cout << endl;
-  }
-}
-void printVector(vector<int> a) {
-  for (int n : a) {
-    cout << n << " ";
-  }
-  cout << endl;
-}
-
-int main(int argn, char *argv[]) {
-    int num;
-    return 0; 
-}
-
-void printVect(vector<unsigned> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << a[i] << " ";
-  }
-  cout << endl;
-}
-void printVect2D(vector<pair<pair<int, int>, pair<int, int>>> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << "(" << a[i].first.first << "," << a[i].first.second << ") " << "("
-         << a[i].second.first << "," << a[i].second.second << ") " << endl;
-  }
-}
-void printVectSS(vector<pair<pair<int, int>, int>> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << a[i].first.first << " " << a[i].first.second << " > " << a[i].second
-         << endl;
-  }
-}
-
-void printprimefactors(vector<pair<int, int>> a) {
-  for (auto pr : a) {
-    cout << pr.first << "^" << pr.second << " ";
-  }
-  cout << endl;
-}
-
-
-
 
 struct ListNode {
   int val;
@@ -105,3 +54,4 @@ public:
         return newHead;
     } // O(n) and O(n) memory as make n temp
 };
+
