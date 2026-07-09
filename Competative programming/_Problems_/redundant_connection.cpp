d7321534404d82b68285ef1c82f0ec0092133a18
SachinyadavAug20 <samtagon777@gmail.com>
2026-07-09 23:04:23 +0530

    Solved: find the town judge

---
+++ Competative programming/_Problems_/redundant_connection.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+  // remove a edge from graph to make a tree
+  // Graph theory -> 
+  // there are many correct ans(not all)
+  // detect a cycle and break it
+  // gurranted cycle get all edges of the cycle
+  // use disjoint set => use union find
+  //
+  vector<int> findRedundantConnection(vector<vector<int>> &edges) {
+  }
+};
