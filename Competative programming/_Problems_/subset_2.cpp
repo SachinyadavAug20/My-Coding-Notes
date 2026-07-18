64583d32816fbde0f5a9f84794553483ebfffb02
SachinyadavAug20 <samtagon777@gmail.com>
2026-07-18 18:53:13 +0530

    Solved: subset part 2

---
+++ Competative programming/_Problems_/subset_2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+  // as nums can have duplicates can't do simple 2^n possible
+  vector<vector<int>> ans;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+      ans.clear();
+      
+    }
+};
