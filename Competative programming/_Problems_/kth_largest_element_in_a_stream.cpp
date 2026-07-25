f9fd3583eea02c7a37b5bc4434b92387b1c62c8d
SachinyadavAug20 <samtagon777@gmail.com>
2026-07-25 20:13:27 +0530

    Solved: kth largest element in stream

---
+++ Competative programming/_Problems_/kth_largest_element_in_a_stream.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+#define ll long long
+
+class KthLargest_ON {
+public:
+    int k;
+    vector<int> topK;
+    int kth;
+
+    KthLargest_ON(int k, vector<int>& nums) {
+        this->k = k;
+        sort(nums.begin(), nums.end());
+        int take = min((int)nums.size(), k);
+        topK = vector<int>(nums.end() - take, nums.end());
+        if (!topK.empty())
+            kth = topK[0];
+    }
+
+    int add(int val) {
+        if (topK.size() < k) {
+            int pos = lower_bound(topK.begin(), topK.end(), val) - topK.begin();
+            topK.insert(topK.begin() + pos, val);
+            kth = topK[0];
+            return kth;
+        }
+        if (val <= kth)
+            return kth;
+        int pos = lower_bound(topK.begin(), topK.end(), val) - topK.begin();
+        topK.erase(topK.begin());
+        pos--;
+        if (pos < 0) pos = 0;
+        if (pos > topK.size()) pos = topK.size();
+        topK.insert(topK.begin() + pos, val);
+        kth = topK[0];
+        return kth;
+    }// O(n)
+};
+
+class KthLargest {
+public:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int kth;
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        int take = min((int)nums.size(), k);
+        sort(nums.begin(), nums.end());
+        pq=priority_queue<int, vector<int>, greater<int>>(nums.end()-take,nums.end());
+    }
+
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>k){
+            pq.pop();
+        }
+        return pq.top();
+    }// O(log(n))
+};
