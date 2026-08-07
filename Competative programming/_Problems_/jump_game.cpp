bbfa1e588c9affaf2c903c90a3804076e44e46f2
SachinyadavAug20 <samtagon777@gmail.com>
2026-08-07 21:55:22 +0530

    Solved: jump game part 1

---
+++ Competative programming/_Problems_/jump_game.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+  bool canJump(vector<int> &nums) {
+    int farthest = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (i > farthest)
+        return false;
+      farthest = max(farthest, i + nums[i]);
+      if (farthest >= nums.size() - 1)
+        return true;
+    }
+    return true;
+  }
+};
