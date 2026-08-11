4bcb9dc30d6c0cf9aa5ef4c50b0f2c67bb82bde1
SachinyadavAug20 <samtagon777@gmail.com>
2026-08-11 21:37:48 +0530

    Solved: hand of straights

--- Competative programming/_Problems_/hand_of_straignts.cpp
+++ Competative programming/_Problems_/hand_of_straignts.cpp
@@ -4,7 +4,28 @@ using namespace std;
 
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        
+  bool isNStraightHand(vector<int> &hand, int groupSize) {
+    if (hand.size() % groupSize != 0)
+      return false;
+    unordered_map<int, int> hsh;
+    for (int card : hand)
+      hsh[card]++;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    for (auto &[card, freq] : hsh)
+      minHeap.push(card);
+    while (!minHeap.empty()) {
+      int first = minHeap.top();
+      for (int i = first; i < first + groupSize; i++) {
+        if (!hsh.count(i))
+          return false;
+        hsh[i]--;
+        if (hsh[i] == 0) {
+          if (i != minHeap.top())
+            return false;
+          minHeap.pop();
+        }
+      }
     }
+    return true;
+  }
 };
