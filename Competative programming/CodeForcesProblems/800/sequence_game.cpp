5d39f29dbdbe256892c860ef94e58c4e638f23cf
SachinyadavAug20 <samtagon777@gmail.com>
2026-09-17 16:17:11 +0530

    Solved: sequence game 800CF

---
+++ Competative programming/CodeForcesProblems/800/sequence_game.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+#define fastio() (ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr))
+#define MOD 1000000007
+#define INF ((long long)1e18)
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define int long long
+
+// obs
+// 1. insert iff a[i-1]>a[i] then inset 1 as it is garented smaller
+
+signed main() {
+    fastio();
+    int q;
+    cin>>q;
+    while (q--) {
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        vector<int> ans;
+        ans.push_back(a[0]);
+        for(int i=1;i<n;i++){
+            if(a[i-1]<=a[i]){
+                ans.push_back(a[i]);
+            }else {
+                ans.push_back(1);
+                ans.push_back(a[i]);
+            }
+        }
+
+        cout<<ans.size()<<endl;
+        for(int i:ans) cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
