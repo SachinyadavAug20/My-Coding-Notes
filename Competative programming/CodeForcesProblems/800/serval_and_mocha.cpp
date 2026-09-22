708f856a4b36f80c77da75c1cebf97d3229b65e7
SachinyadavAug20 <samtagon777@gmail.com>
2026-09-22 22:27:15 +0530

    Solved: serval and mocha array GCD 800CF

--- Competative programming/CodeForcesProblems/800/serval_and_mocha.cpp
+++ Competative programming/CodeForcesProblems/800/serval_and_mocha.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio() (ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr))
+#define fastio()                                                               \
+  (ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr))
 #define MOD 1000000007
 #define INF ((long long)1e18)
 #define endl '\n'
@@ -10,8 +11,33 @@ using namespace std;
 #define ff first
 #define ss second
 #define int long long
+// obs
+// adding numbers for GCD -> will stay gcd constant or increase it(never
+// decrease)
 
 signed main() {
-    fastio();
-    return 0;
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+    }
+    bool possible = false;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (gcd(a[i], a[j]) <= 2) {
+          possible = true;
+          break;
+        }
+      }
+      if (possible)
+        break;
+    }
+    if (possible) cout << "YES\n";
+    else cout << "NO\n";
+  }
+  return 0;
 }
