238a730222455bf8c0a071c061646491c49234ed
SachinyadavAug20 <samtagon777@gmail.com>
2026-09-25 22:14:52 +0530

    Solved: make it beautiful 800CF

--- Competative programming/CodeForcesProblems/800/make_it_beautiful.cpp
+++ Competative programming/CodeForcesProblems/800/make_it_beautiful.cpp
@@ -1,23 +1,36 @@
-#include <bits/stdc++.h>
-#include <mutex>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-#define fastio() (ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr))
-#define MOD 1000000007
-#define INF ((long long)1e18)
-#define endl '\n'
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define int long long
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
 
-signed main() {
-    int t;
-    cin>>t;
-    while (t--) {
-        int n;
-        cin>>n;
-    }
-    return 0;
+  if (a[0] == a[n - 1]) {
+    cout << "NO" << endl;
+    return;
+  }
+
+  swap(a[0], a[n - 1]);
+
+  cout << "YES" << endl;
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << (i == n - 1 ? "" : " ");
+  }
+  cout << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
 }
